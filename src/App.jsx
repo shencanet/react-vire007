5d887980ec003c8966a9e871ca918897819ac66e
shen <shencanet@gmail.com>
2024-03-16 22:17:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+    return (
+        <div>
+        <h1>HOLA ME LLAMO SANTI</h1>
+        <h1>HOLA ME LLAMO SANTI</h1>
+        <img src="https://picsum.photos/200/300" alt="imagen random"></img>
+
+      
+        </div>
+    );
+    }
+
+export default App;
